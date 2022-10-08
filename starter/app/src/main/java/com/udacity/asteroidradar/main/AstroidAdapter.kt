ec6f590a2b6ae175b01132b40a334152dde6408b
AdhamNour <adhamnourelwaffaa@gmail.com>
2022-10-08 21:38:46 +0200

    refactored onCReateView and onBindViewHolder

--- starter/app/src/main/java/com/udacity/asteroidradar/main/AstroidAdapter.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/main/AstroidAdapter.kt
@@ -8,29 +8,41 @@ import androidx.recyclerview.widget.RecyclerView
 import com.udacity.asteroidradar.Asteroid
 import com.udacity.asteroidradar.R
 
-class AstroidAdapter: RecyclerView.Adapter<AsteroidViewHolder>() {
+class AstroidAdapter : RecyclerView.Adapter<AsteroidViewHolder>() {
     var data = listOf<Asteroid>()
-    set(value) {
-        field=value;
-        notifyDataSetChanged()
-    }
+        set(value) {
+            field = value;
+            notifyDataSetChanged()
+        }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AsteroidViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater
-            .inflate(R.layout.astroid_list_itm, parent, false)
-        return AsteroidViewHolder(view)
+
+        return AsteroidViewHolder.from(parent)
 
 
     }
 
     override fun onBindViewHolder(holder: AsteroidViewHolder, position: Int) {
         val item = data[position]
-        holder.nameTextView.text=item.codename
+        holder.bind(item)
     }
 
-    override fun getItemCount()=data.size;
+    override fun getItemCount() = data.size;
 }
 
-class AsteroidViewHolder(itemView: View) :RecyclerView.ViewHolder(itemView){
-    val nameTextView= itemView.findViewById<TextView>(R.id.name)
+class AsteroidViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val nameTextView = itemView.findViewById<TextView>(R.id.name)
+
+    fun bind(asteroid: Asteroid) {
+        nameTextView.text = asteroid.codename
+    }
+
+    companion object {
+        fun from(parent:ViewGroup):AsteroidViewHolder{
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val view = layoutInflater
+                .inflate(R.layout.astroid_list_itm, parent, false)
+            return AsteroidViewHolder(view)
+        }
+    }
 }
