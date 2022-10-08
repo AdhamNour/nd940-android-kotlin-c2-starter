ae76620259de06e3123c5f3a03375695b2330e94
AdhamNour <adhamnourelwaffaa@gmail.com>
2022-10-08 23:05:43 +0200

    finihsed the conversion between the db model and the domain model

---
+++ starter/app/src/main/java/com/udacity/asteroidradar/database/DatabaseAstroid.kt
@@ -0,0 +1,43 @@
+package com.udacity.asteroidradar.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.udacity.asteroidradar.Asteroid
+
+@Entity
+class DatabaseAstroid(
+    @PrimaryKey val id: Long, val codename: String, val closeApproachDate: String,
+    val absoluteMagnitude: Double, val estimatedDiameter: Double,
+    val relativeVelocity: Double, val distanceFromEarth: Double,
+    val isPotentiallyHazardous: Boolean
+)
+
+fun List<DatabaseAstroid>.asDomainAstroids(): List<Asteroid> {
+    return map {
+        Asteroid(
+            id = it.id,
+            codename = it.codename,
+            closeApproachDate = it.closeApproachDate,
+            absoluteMagnitude = it.absoluteMagnitude,
+            estimatedDiameter = it.estimatedDiameter,
+            relativeVelocity = it.relativeVelocity,
+            distanceFromEarth = it.distanceFromEarth,
+            isPotentiallyHazardous = it.isPotentiallyHazardous
+        )
+    }
+}
+
+fun List<Asteroid>.asDatabaseAstroid():Array<DatabaseAstroid> {
+    return map {
+        DatabaseAstroid(
+            id = it.id,
+            codename = it.codename,
+            closeApproachDate = it.closeApproachDate,
+            absoluteMagnitude = it.absoluteMagnitude,
+            estimatedDiameter = it.estimatedDiameter,
+            relativeVelocity = it.relativeVelocity,
+            distanceFromEarth = it.distanceFromEarth,
+            isPotentiallyHazardous = it.isPotentiallyHazardous
+        )
+    }.toTypedArray()
+}
