739b2875c44de4265fcc07610caa09be732e39ee
Adham Nour <adhamnourelwaffaa@gmail.com>
2022-10-15 12:56:56 +0200

    Added Filter

--- starter/app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
@@ -1,16 +1,21 @@
 package com.udacity.asteroidradar.main
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.*
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import com.udacity.asteroidradar.R
 import com.udacity.asteroidradar.databinding.FragmentMainBinding
 import timber.log.Timber
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class MainFragment : Fragment() {
 
@@ -43,7 +48,17 @@ class MainFragment : Fragment() {
         super.onCreateOptionsMenu(menu, inflater)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.show_all_menu -> viewModel.toggleWeekFilter()
+            R.id.show_rent_menu -> {
+                viewModel.toggleFilterToday()
+
+
+            }
+            R.id.show_buy_menu -> view?.let { Snackbar.make(it,"All Asteroid are saved periodically,so there would be no unsaved Asteroid", Snackbar.LENGTH_SHORT).show() }
+        }
         return true
     }
 }
--- starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,29 +1,41 @@
 package com.udacity.asteroidradar.main
 
 import android.app.Application
-import android.util.Log
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.*
 import com.udacity.asteroidradar.Asteroid
 import com.udacity.asteroidradar.PictureOfDay
 import com.udacity.asteroidradar.api.NasaApi
-import com.udacity.asteroidradar.api.parseAsteroidsJsonResult
 import com.udacity.asteroidradar.database.getDatabase
 import com.udacity.asteroidradar.repository.AstroidRepository
 import kotlinx.coroutines.launch
-import org.json.JSONObject
-import timber.log.Timber
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
+@RequiresApi(Build.VERSION_CODES.O)
 class MainViewModel (application: Application) : AndroidViewModel(application) {
-    private val _pictureOfDay = MutableLiveData<PictureOfDay>();
+    private val _pictureOfDay = MutableLiveData<PictureOfDay>()
     val pictureOfDay: LiveData<PictureOfDay>
         get() = _pictureOfDay
-    private val database = getDatabase(application);
+    private val database = getDatabase(application)
     private val asteroidRepository = AstroidRepository(database)
+
+    private val todayFilter = MutableLiveData(false)
+    private val weekFilter = MutableLiveData(false)
+
+    private val _asteroidArray: LiveData<List<Asteroid>> = asteroidRepository.asteroids
+
+    private val _filteredAsterids = MutableLiveData<List<Asteroid>>()
+
     val asteroidArray: LiveData<List<Asteroid>>
-        get() = asteroidRepository.asteroids
+        get() = _filteredAsterids
 
     init {
         getImageOfTheDay()
+        todayFilter.observeForever { filterAsteroids() }
+        weekFilter.observeForever { filterAsteroids() }
+        _asteroidArray.observeForever { filterAsteroids() }
     }
 
 
@@ -38,5 +50,33 @@ class MainViewModel (application: Application) : AndroidViewModel(application) {
         }
     }
 
+     fun toggleFilterToday (){
+        todayFilter.value= !todayFilter.value!!
+    }
 
+
+    fun toggleWeekFilter(){
+        weekFilter.value = !weekFilter.value!!
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun filterAsteroids(){
+        val filteredAsteroids = mutableListOf<Asteroid>()
+        if(todayFilter.value == true)
+            _asteroidArray.value?.forEach {
+            val localDate = LocalDate.parse(it.closeApproachDate, DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+            if(localDate.equals(LocalDate.now()))
+                filteredAsteroids.add(it)
+        }
+        else if (weekFilter.value== true)
+            _asteroidArray.value?.forEach {
+                val localDate = LocalDate.parse(it.closeApproachDate, DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+                if(localDate.isAfter(LocalDate.now().minusDays(7)))
+                    filteredAsteroids.add(it)
+            }
+        else _asteroidArray.value?.forEach {
+            filteredAsteroids.add(it)
+        }
+    _filteredAsterids.value=filteredAsteroids
+    }
 }
